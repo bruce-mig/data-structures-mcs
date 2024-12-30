a759b08fccc74c66c68ef3fcb67b25db219423c5
bruce-mig <brucetmig@icloud.com>
2024-12-30 22:27:07 +0200

    array implementation of stack

---
+++ stack-adt/stack_array_impl.c
@@ -0,0 +1,49 @@
+// Stack - Array based implementation
+#include <stdio.h>
+#define MAX_SIZE 101
+
+int A[MAX_SIZE];
+int topVal = -1; // empty stack
+
+void push(int x);
+void pop();
+void print();
+
+int main() {
+    push(2); print();
+    push(5); print();
+    push(10); print();
+    pop(); print();
+    push(12); print();
+}
+
+void push(int x) {
+    if (topVal == MAX_SIZE -1) {
+        printf("Error: Stack overflow\n");
+        return;
+    }
+
+    A[++topVal] = x;
+}
+
+void pop() {
+    if (topVal == -1) {
+        printf("Error: No element to pop\n");
+        return;
+    }
+
+    topVal--;
+}
+
+int top() {
+    return A[topVal];
+}
+
+void print() {
+    int i;
+    printf("Stack: ");
+    for (i = 0; i <= topVal; i++) {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
