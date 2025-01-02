e80e957396330b4eaf2beb9b747879dc28f61407
bruce-mig <brucetmig@icloud.com>
2025-01-02 19:08:17 +0200

    binary search tree implementation

---
+++ binary_search_tree/binary_search_tree.cpp
@@ -0,0 +1,57 @@
+// Binary Search Tree -Implementation in C++
+#include <iostream>
+using namespace std;
+
+struct BstNode {
+    int data;
+    BstNode *left;
+    BstNode *right;
+};
+
+
+BstNode* getNewNode(int data);
+BstNode* insert(BstNode *root,int data);
+bool search(BstNode *root, int data);
+
+int main(int argc, char *argv[]) {
+    BstNode *root = NULL; //creating an empty tree
+
+    for (int i = 1; i < argc; i++) {
+        int number = atoi(argv[i]);
+        insert(root,number);
+    }
+
+    int number;
+    cout<<"Enter number to be searched\n";
+    cin>>number;
+    if(search(root, number) == true) cout<<"Found\n";
+    else cout<<"Not Found\n";
+
+}
+
+BstNode* insert(BstNode *root,int data) {
+    if (root== NULL) {
+        root = getNewNode(data);
+    } else if (data <= root->data) {
+        root->left = insert(root->left, data);
+    } else {
+        root->right = insert(root->right, data);
+    }
+    return root;
+
+}
+
+bool search(BstNode *root, int data) {
+    if(root == NULL) return false;
+    else if(root->data == data) return true;
+    else if(data <= root->data) return search(root->left,data);
+    else return search(root->right,data);
+
+}
+
+BstNode* getNewNode(int data) {
+    BstNode *newNode = new BstNode();
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
